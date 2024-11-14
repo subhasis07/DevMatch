37c5612fb3a0fa727103b99d0a5d2f7eb16dbde6
Subhasis Pal <palsubhasis100@gmail.com>
2024-11-14 22:35:09 +0530

    Added Schema_validation & API_Sanitization

--- src/app.js
+++ src/app.js
@@ -4,14 +4,19 @@ const User = require('./models/users');
 
 const app=express();
 
+app.use(express.json()); //Middleware to convert JSON data to js Obj & it will run in every call as no route is mentioned externally
 
 app.post("/signup", async(req,res)=>{
-    const user= new User({
-        firstName: "Subhasis",
-        lastName:"Pal",
-        emailID:"abc@gmail.com",
-        password:"sub@123",
-    })
+
+    const user= new User(req.body);
+
+    //Creatung instance of user model
+    // const user= new User({
+    //     firstName: "Subhasis",
+    //     lastName:"Pal",
+    //     emailID:"abc@gmail.com",
+    //     password:"sub@123",
+    // })
 
     try {
         await user.save();
@@ -22,6 +27,71 @@ app.post("/signup", async(req,res)=>{
     
 })
 
+app.get("/user", async(req,res)=>{
+    const userEmail=req.body.emailID;
+    try {
+        const users= await User.find({emailID:userEmail}); //findOne will result only the 1st one if there are same result of same filter
+        if(users.length===0){
+            res.status(404).send("No user found");
+        }else{
+            res.send(users);
+        }
+        
+    } catch (error) {
+        res.status(404).send("Something went wrong");
+    }
+})
+
+app.get("/feed",async(req,res)=>{
+    try {
+        const users=await User.find({});
+        if(users.length===0){
+            res.status(404).send("No user found");
+        }else{
+            res.send(users);
+        }
+    } catch (error) {
+        res.status(404).send("Something went wrong");
+    }
+})
+
+app.delete("/user", async(req,res)=>{
+    const userId= req.body.userId;
+    try{
+        const user= await User.findByIdAndDelete(userId);
+        // const user= await User.findByIdAndDelete({_id:userId});
+        res.send("User deleted successfully");
+    }catch (error) {
+        res.status(404).send("Something went wrong");
+    }
+})
+
+app.patch("/user/:userId",async(req,res)=>{
+    const userUpdate=req.body;
+    const userId=req.params.userId;
+
+    try {
+        const ALLOWED_DATA=["skills","photoURL","about"];
+        const isUpdateAllowed=Object.keys(userUpdate).every((key)=>{
+            ALLOWED_DATA.includes(key);
+        })
+        if(!isUpdateAllowed){
+            throw new Error("Update not Allowed")
+        }
+        if(userUpdate?.skills.length>10){
+            throw new Error("Skills should not be >10")
+        }
+
+        const user= await User.findByIdAndUpdate({_id:userId}, userUpdate, {
+            returnDocument:"before",
+            runValidators:true,
+        }); // the 3rd parameter is optional if nothing given & next line we log the user --> it will display the result before the update was done..
+        console.log(user);
+        res.send("user Updated successfully")
+    } catch (error) {
+        res.status(404).send("Something went wrong");
+    }
+})
 connectDB()
     .then(()=>{
         console.log("DB connected");
--- src/models/users.js
+++ src/models/users.js
@@ -1,24 +1,65 @@
 const mongoose= require('mongoose');
-
+const validate=require('validator')
 const userSchema=new mongoose.Schema({
     firstName:{
         type:String,
+        required:true,
+        minLength:3
     },
     lastName:{
         type:String,
     },
     emailID:{
         type:String,
+        required: true,
+        lowercase:true,
+        unique:true,
+        trim:true,
+        validate(value){
+            if(!validate.isEmail(value)){
+                throw new Error("Invalid Email ID")
+            }
+        }
     },
     password:{
         type:String,
+        required: true,
+        validate(value){
+            if(!validate.isStrongPassword(value)){
+                throw new Error("Enter a string Password")
+            }
+        }
     },
     age:{
         type:Number,
+        min:17
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){  // by default this validator will work only during creating new user ; to apply this validators during updating user, we need to apply options in app.js->patch api -> runValidators:true
+            if(!["male","female", "other"]){
+                throw new Error("Gender not found");
+            }
+        }
+    },
+    about:{
+        type:String,
+        default:" Default About",
+    },
+    photoURL:{
+        type:String,
+        default: "https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg",
+        validate(value){
+            if(!validate.isURL(value)){
+                throw new Error("Invalid Photo URL")
+            }
+        }
+    },
+    skills:{
+        type:[String]
     }
+},{
+    timestamps:true,
 })
 
 const User = mongoose.model("User",userSchema);
